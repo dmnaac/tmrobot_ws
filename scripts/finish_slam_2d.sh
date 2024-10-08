a26ce23d73360e8a47f0a84c42c27f3dd3544146
dming <dmnaac@hotmail.com>
2024-10-08 11:29:15 +0800

    Modified.

--- scripts/finish_slam_2d.sh
+++ scripts/finish_slam_2d.sh
@@ -3,12 +3,11 @@
 # Get the current date and time
 current_date_time=$(date +"%Y-%m-%d_%H-%M-%S")
 
-map_root="${HOME}/cartographer_ws/map"
+map_root="${HOME}/Workspace/cartographer_ws/map"
 map_name="$current_date_time"
 
-lua_root="${HOME}/cartographer_ws/cartographer_ros_ws/src/
-         cartographer_ros/cartographer_ros/configuration_files"
-lua_name="tmrobot_2d.lua"
+# lua_root="${HOME}/Workspace/cartographer_ws/cartographer_ros_ws/src/cartographer_ros/cartographer_ros/configuration_files"
+# lua_name="cartographer_ros_sim.lua"
 
 # Check if map folder exists, if not create one.
 if [ ! -d "$map_root" ];then
@@ -21,11 +20,11 @@ map_dir="$map_root/$map_name"
 # Create a directory with the current date and time as its name
 mkdir "$map_dir"
 
-lua_dir="$lua_root/$lua_name"
+# lua_dir="$lua_root/$lua_name"
 
 # Copy the lua file to the new map folder
-cp "$lua_dir" "$map_dir"
-echo "lua file for this map has been copied"
+# cp "$lua_dir" "$map_dir"
+# echo "lua file for this map has been copied"
 
 # Finish slam
 rosservice call /finish_trajectory 0
